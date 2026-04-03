ea6645e634e04481abb10c225f399ceb81bc7029
krackxor <77487814+krackxor@users.noreply.github.com>
2026-04-04 04:14:03 +0700

    Fix import path for wait_for_message function

--- bot/callbacks.py
+++ bot/callbacks.py
@@ -37,7 +37,7 @@ from bot_helper.Telegram.Telegram_Client import Telegram
 from config.config import Config
 
 # Memanggil fitur "Inline Waiter" dari shared.py
-from .shared import wait_for_message
+from bot.shared import wait_for_message
 
 SAVE_TO_DATABASE = Config.SAVE_TO_DATABASE
 LOGGER           = Config.LOGGER
